27e3f2a408458cbbbca6f95b9b15175f65f58e91
whit <whit@openplans.org>
2009-05-27 17:34:08 -0400

    update pavement

--- Resources/pavement.py
+++ Resources/pavement.py
@@ -16,6 +16,7 @@ except :
     # minilib does not support bootstrap
     pass
 
+from ConfigParser import ConfigParser
 from paver import setuputils
 from paver.easy import *
 from paver.easy import call_task #debug,
@@ -40,13 +41,61 @@ version = "0.0"
 curdir = os.path.abspath(os.curdir)
 
 options(
-    virtualenv=Bunch(script_name="build_app",
+    virtualenv=Bunch(script_name="build_app_env",
                      #packages_to_install=['supervisor',
                      #                     'tempita',
                      #                     'paste',
                      #                     'jstools'],
                      paver_command_line="after_bootstrap"
                      ),
-#    sphinx=Bunch(docroot="src/trunk/docsrc",
-#                 builddir=path(curdir) / "built")
+               #    sphinx=Bunch(docroot="src/trunk/docsrc",
+               #                 builddir=path(curdir) / "built")
     )
+
+APPCONF = "app-conf.cfg"
+_DIRS = "src", "var", "etc", "var/logs"
+
+@task
+def auto(default_conf=APPCONF):
+    env = os.environ.get('VIRTUAL_ENV')
+    if env is None:
+        env = path("./").abspath()
+
+    cp = ConfigParser()
+    cp.read(default_conf)
+    options(config=cp,
+            env=env,
+            conf_fp=default_conf)
+
+
+@task
+@needs(['auto'])
+def after_bootstrap():
+    call_task("dir_layout")
+
+
+@task
+def dir_layout():
+    env = path(options.env)
+    for p in _DIRS:
+        if not (env / p).exists():
+            os.mkdir(env / p)
+
+# 0. install py packages
+
+# 1.
+# setup supervisor
+# setup proxy
+# setup geoserver
+
+# 2.
+# ui for finding data
+# ui for starting and restart processes
+# geoexplorer
+
+# 3.
+# geobuilder
+
+# 4.
+# postgis of love
+
